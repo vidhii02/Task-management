740998a77d1f78cfe8d9dab50bf1f162d40ec7b1
vidhii02 <kodarlikarvidhi02@gmail.com>
2022-09-23 16:33:44 +0530

    added js to the website

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,39 @@
-console.log("hello world");
+divdivconst state={
+    taskList:[],
+};
+
+//DOM - document object
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task_modal_body");
+
+const htmlTaskContents=({id,title, description, type, url})=>`
+    <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
+        <div class='card shaodow-sm task__card'>
+            <div class='card-header d-flex gap-2 justify-content-end task__card__header'>
+            <button type='button' class='btn btn-outline-info mr-2' name=${id}>
+            <i class='fas fa-pencil-alt' name=${id}></i>
+            </button>
+            <button type='button' class='btn btn-outline-danger mr-2' name=${id}>
+            <i class='fas fa-trash-alt' name=${id}></i>
+            </button>
+            </div>
+                <div class='card-body>
+                ${
+                    url && `<img width='100%' src=${url} alt='card image cap' class='card-image-top md-3 rounded-lg'/>`
+                }
+                <h4 class='task__card__title'>${title}</h4>   
+                <p class='description trim-3-lines text-muted' data-gram_editor='false'>
+                ${description}</p>  
+                <div class=' tags text-white d-flex flex-wrap'>
+                <span class='badge bg-primary m-1'>${type}</span>
+                </div>  
+                <div class='card-footer'>
+                <button type='button class='btn btn-outline-primaryy float-right' data-bs-toggle='modal'
+                data-bs-target='#showTask'>Open Task</button> 
+                </div>
+            </div>
+        </div>
+    </div>
+`;
+
+const htmlModalCoten
